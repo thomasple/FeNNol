fd7392aa39f9ea91f005a959ffc2dc1a9e6bf75c
Thomas PLE <thomas.ple@hotmail.fr>
2023-11-02 13:35:14 +0100

    Add new activation functions: TSSR and TSSR2

--- src/fennol/utils/activations.py
+++ src/fennol/utils/activations.py
@@ -1,6 +1,8 @@
 import jax
+import jax.numpy as jnp
 from functools import partial
 import math
+
 
 @jax.jit
 def aptx(x):
@@ -12,11 +14,34 @@ def leaky_celu(x, alpha=0.1):
     return alpha * x + (1.0 - alpha) * (jax.nn.softplus(x) - math.log(2.0))
 
 
+@jax.jit
+def tssr(x):
+    ax = jnp.abs(x)
+    mask = ax <= 1.0
+    axx = jnp.where(mask, 1.0, ax)
+    return jnp.where(mask, x, jnp.sign(x) * (2 * axx**0.5 - 1))
+
+
+@jax.jit
+def tssr2(x):
+    ax = jnp.abs(x)
+    mask = ax <= 1.0
+    axx = jnp.where(mask, 1.0, ax)
+    return jnp.sign(x) * jnp.where(mask, 1.25 * ax - 0.25 * ax**3, axx**0.5)
+
+
 def activation_from_str(activation: str):
     if activation is None or activation.lower() == "none":
         return lambda x: x
     return eval(
         activation,
         {"__builtins__": None},
-        {**jax.nn.__dict__, "partial": partial, "leaky_celu": leaky_celu, "aptx": aptx},
+        {
+            **jax.nn.__dict__,
+            "partial": partial,
+            "leaky_celu": leaky_celu,
+            "aptx": aptx,
+            "tssr": tssr,
+            "tssr2": tssr2,
+        },
     )
