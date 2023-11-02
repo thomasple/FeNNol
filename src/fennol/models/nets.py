845184390fc09f7bff83780576979a16e707df98
Thomas PLE <thomas.ple@hotmail.fr>
2023-11-02 13:34:07 +0100

    Add new neural network modules including SkipNet, HierarchicalNet, and ResMLP

--- src/fennol/models/nets.py
+++ src/fennol/models/nets.py
@@ -52,16 +52,163 @@ class FullyConnectedNet(nn.Module):
             else self.activation
         )
         ############################
-        for i,d in enumerate(self.neurons[:-1]):
+        for i, d in enumerate(self.neurons[:-1]):
             x = nn.Dense(d, use_bias=self.use_bias, name=f"Layer_{i+1}")(x)
             x = activation(x)
-        x = nn.Dense(self.neurons[-1], use_bias=self.use_bias, name=f"Layer_{len(self.neurons)}")(x)
+        x = nn.Dense(
+            self.neurons[-1], use_bias=self.use_bias, name=f"Layer_{len(self.neurons)}"
+        )(x)
         ############################
 
         if self.input_key is not None:
             output_key = self.name if self.output_key is None else self.output_key
             return {**inputs, output_key: x} if output_key is not None else x
         return x
+
+class ResMLP(nn.Module):
+    """
+        Residual neural network as defined in SpookyNet paper
+    """
+    output_dim: int
+    activation: Union[Callable, str] = nn.silu
+    use_bias: bool = True
+    input_key: Optional[str] = None
+    output_key: Optional[str] = None
+
+    @nn.compact
+    def __call__(self, inputs: Union[dict, jax.Array]) -> Union[dict, jax.Array]:
+        if self.input_key is None:
+            assert not isinstance(
+                inputs, dict
+            ), "input key must be provided if inputs is a dictionary"
+            x = inputs
+        else:
+            x = inputs[self.input_key]
+
+        activation = (
+            activation_from_str(self.activation)
+            if isinstance(self.activation, str)
+            else self.activation
+        )
+        ############################
+        y = nn.Dense(x.shape[-1],use_bias=self.use_bias)(activation(x))
+        y = activation(x + nn.Dense(x.shape[-1],use_bias=self.use_bias)(activation(y)))
+        x = nn.Dense(
+            self.output_dim, use_bias=self.use_bias
+        )(y)
+        ############################
+
+        if self.input_key is not None:
+            output_key = self.name if self.output_key is None else self.output_key
+            return {**inputs, output_key: x} if output_key is not None else x
+        return x
+
+
+class FullyResidualNet(nn.Module):
+    """
+    A fully connected neural network module with residual connections.
+
+    Args:
+        neurons (Sequence[int]): A sequence of integers representing the dimensions of the network.
+        activation (Callable, optional): The activation function to use. Defaults to nn.silu.
+        use_bias (bool, optional): Whether to use bias in the dense layers. Defaults to True.
+        input_key (Optional[str], optional): The key to use to access the input tensor. Defaults to None.
+        output_key (Optional[str], optional): The key to use to access the output tensor. Defaults to None.
+    """
+
+    dim: int
+    output_dim:int
+    nlayers: int
+    activation: Union[Callable, str] = nn.silu
+    use_bias: bool = True
+    input_key: Optional[str] = None
+    output_key: Optional[str] = None
+
+    @nn.compact
+    def __call__(self, inputs: Union[dict, jax.Array]) -> Union[dict, jax.Array]:
+        """
+        Applies the neural network to the given inputs.
+
+        Args:
+        - inputs: A dictionary or JAX array containing the inputs to the neural network.
+
+        Returns:
+        - A dictionary or JAX array containing the output of the neural network.
+        """
+        if self.input_key is None:
+            assert not isinstance(
+                inputs, dict
+            ), "input key must be provided if inputs is a dictionary"
+            x = inputs
+        else:
+            x = inputs[self.input_key]
+
+        activation = (
+            activation_from_str(self.activation)
+            if isinstance(self.activation, str)
+            else self.activation
+        )
+        ############################
+        if x.shape[-1] != self.dim:
+            x = nn.Dense(self.dim, use_bias=self.use_bias, name=f"Reshape")(x)
+
+        for i in range(self.nlayers - 1):
+            x = x + activation(
+                nn.Dense(self.dim, use_bias=self.use_bias, name=f"Layer_{i+1}")(x)
+            )
+        x = nn.Dense(
+            self.output_dim, use_bias=self.use_bias, name=f"Layer_{self.nlayers}"
+        )(x)
+        ############################
+
+        if self.input_key is not None:
+            output_key = self.name if self.output_key is None else self.output_key
+            return {**inputs, output_key: x} if output_key is not None else x
+        return x
+
+
+class HierarchicalNet(nn.Module):
+    """
+    Neural network for a sequence of inputs (in axis=-2) with a decay factor
+    """
+
+    neurons: Sequence[int]
+    activation: Union[Callable, str] = nn.silu
+    use_bias: bool = True
+    input_key: Optional[str] = None
+    output_key: Optional[str] = None
+    decay: float = 0.01
+
+    @nn.compact
+    def __call__(self, inputs: Union[dict, jax.Array]) -> Union[dict, jax.Array]:
+        if self.input_key is None:
+            assert not isinstance(
+                inputs, dict
+            ), "input key must be provided if inputs is a dictionary"
+            x = inputs
+        else:
+            x = inputs[self.input_key]
+
+        ############################
+        networks = nn.vmap(
+            FullyConnectedNet,
+            variable_axes={"params": 0},
+            split_rngs={"params": True},
+            in_axes=-2,
+            out_axes=-2,
+        )(self.neurons, self.activation, self.use_bias)
+
+        out = networks(x)
+        # scale each layer by a decay factor
+        decay = jnp.asarray([self.decay**i for i in range(out.shape[-2])])
+        out = out * decay[..., :, None]
+
+        ############################
+
+        if self.input_key is not None:
+            output_key = self.name if self.output_key is None else self.output_key
+            return {**inputs, output_key: out} if output_key is not None else out
+        return out
 
 
 class ChemicalNetHet(nn.Module):
@@ -179,7 +326,7 @@ class ChemicalNet(nn.Module):
 
         ############################
         # build shape-sharing networks using vmap
-        Networks = nn.vmap(
+        networks = nn.vmap(
             FullyConnectedNet,
             variable_axes={"params": 0},
             split_rngs={"params": True},
@@ -190,11 +337,11 @@ class ChemicalNet(nn.Module):
 
         # apply networks to input and select the output corresponding to the species
         out = jnp.squeeze(
-            jnp.take_along_axis(Networks(x), indices[None, :, None], axis=0), axis=0
+            jnp.take_along_axis(networks(x), indices[None, :, None], axis=0), axis=0
         )
         ############################
 
-        out = jnp.where((indices>=0)[:,None], out, 0.0)
+        out = jnp.where((indices >= 0)[:, None], out, 0.0)
 
         if self.input_key is not None:
             output_key = self.name if self.output_key is None else self.output_key
@@ -204,6 +351,9 @@ class ChemicalNet(nn.Module):
 
 NETWORKS = {
     "NEURAL_NET": FullyConnectedNet,
+    "SKIP_NET": FullyResidualNet,
+    "HIERARCHICAL_NET": HierarchicalNet,
     "CHEMICAL_NET": ChemicalNet,
     "CHEMICAL_NET_HET": ChemicalNetHet,
+    "RES_MLP": ResMLP,
 }
