3bf9f58a6d5521acacb6f693f871677d25d8893b
Thomas PLE <thomas.ple@hotmail.fr>
2024-01-25 11:20:11 +0100

    minor modifications

--- src/fennol/models/physics/bond.py
+++ src/fennol/models/physics/bond.py
@@ -25,7 +25,7 @@ class CND4(nn.Module):
     k1: float = 4.1
     k2: float = 19.09
     k3: float = 254.56
-    electronegativity_factor: bool = True
+    electronegativity_factor: bool = False
     trainable: bool = False
 
     @nn.compact
@@ -35,58 +35,75 @@ class CND4(nn.Module):
         species = inputs["species"]
 
         if self.trainable:
-            rc = self.param("rc", lambda key : jnp.asarray(D3_COV_RADII))[species]
+            rc = self.param("rc", lambda key: jnp.asarray(D3_COV_RADII))[species]
         else:
             rc = jnp.asarray(D3_COV_RADII)[species]
         rcij = rc[edge_src] + rc[edge_dst]
-        rij = graph["distances"]/au.BOHR
+        rij = graph["distances"] / au.BOHR
 
-        k0=self.k0
         if self.trainable:
-            k0 = k0*jnp.abs(self.param("k0", lambda key : jnp.asarray(1.)))
+            k0 = self.k0 * jnp.abs(self.param("k0", lambda key: jnp.asarray(1.0)))
+        else:
+            k0 = self.k0
 
-        CNij = 0.5 * (1 + jax.scipy.special.erf(k0 * (1 - rij / rcij))) * graph["switch"]
+        CNij = (
+            0.5 * (1 + jax.scipy.special.erf(k0 * (1 - rij / rcij))) * graph["switch"]
+        )
 
         if self.electronegativity_factor:
             k1 = self.k1
             k2 = self.k2
             k3 = self.k3
             if self.trainable:
-                k1 = k1*jnp.abs(self.param("k1", lambda key : jnp.asarray(1.)))
-                k2 = self.param("k2", lambda key : jnp.asarray(1.))
-                k3 = jnp.abs(self.param("k3", lambda key : jnp.asarray(1.)))
-                en = self.param("en", lambda key : jnp.asarray(D3_ELECTRONEGATIVITIES))[species]
+                k1 = k1 * jnp.abs(self.param("k1", lambda key: jnp.asarray(1.0)))
+                k2 = self.param("k2", lambda key: jnp.asarray(1.0))
+                k3 = jnp.abs(self.param("k3", lambda key: jnp.asarray(1.0)))
+                en = self.param("en", lambda key: jnp.asarray(D3_ELECTRONEGATIVITIES))[
+                    species
+                ]
             else:
                 en = jnp.asarray(PAULING_ELECTRONEGATIVITY)[species]
             en_ij = jnp.abs(en[edge_src] - en[edge_dst])
-            dij = k1 * jnp.exp(-(en_ij + k2) ** 2 / k3)
+            dij = k1 * jnp.exp(-((en_ij + k2) ** 2) / k3)
             CNij = CNij * dij
         CNi = jax.ops.segment_sum(CNij, edge_src, species.shape[0])
 
         output_key = self.name if self.output_key is None else self.output_key
         return {**inputs, output_key: CNi, output_key + "_pair": CNij}
 
+
 class SumSwitch(nn.Module):
     graph_key: str = "graph"
     output_key: Optional[str] = None
+    pow: float = 1.0
+    trainable: bool = False
 
     @nn.compact
     def __call__(self, inputs):
         graph = inputs[self.graph_key]
-        edge_src, edge_dst = graph["edge_src"], graph["edge_dst"]
+        edge_src = graph["edge_src"]
         switch = graph["switch"]
 
-        cn = jax.ops.segment_sum(switch, edge_src, inputs["species"].shape[0])
+        if self.trainable:
+            p = jnp.abs(
+                self.param("pow", lambda key: jnp.asarray(self.pow))
+            )
+        else:
+            p = self.pow
+        shift=(1.e-3)**p
+
+        cn = jax.ops.segment_sum((1.e-3+switch)**p-shift, edge_src, inputs["species"].shape[0])
 
         output_key = self.name if self.output_key is None else self.output_key
         return {**inputs, output_key: cn}
-    
+
+
 class CNShift(nn.Module):
     cn_key: str
     output_key: Optional[str] = None
     kappa_key: Optional[str] = None
-    sqrt_shift: float = 1.e-6
-    ref_value: Union[str, float] = 1.
+    sqrt_shift: float = 1.0e-6
+    ref_value: Union[str, float] = 1.0
     enforce_positive: bool = False
     cn_pow: float = 0.5
 
@@ -98,32 +115,35 @@ class CNShift(nn.Module):
             assert kappai.shape == CNi.shape
         else:
             species = inputs["species"]
-            kappai = self.param("kappa", nn.initializers.zeros, (len(D3_COV_RADII),))[species]
+            kappai = self.param("kappa", nn.initializers.zeros, (len(D3_COV_RADII),))[
+                species
+            ]
         shift = kappai * (CNi + self.sqrt_shift) ** self.cn_pow
-        
-        if isinstance(self.ref_value,str):
+
+        if isinstance(self.ref_value, str):
             ref_value = inputs[self.ref_value]
             assert ref_value.shape == shift.shape
         else:
             ref_value = self.ref_value
 
         if self.enforce_positive:
-            shift = jax.nn.celu(shift,alpha=ref_value)
-        
+            shift = jax.nn.celu(shift, alpha=ref_value)
+
         out = ref_value + shift
 
         output_key = self.name if self.output_key is None else self.output_key
         return {**inputs, output_key: out}
+
 
 class CNStore(nn.Module):
     cn_key: str
     output_key: Optional[str] = None
     store_size: int = 10
     n_gaussians: int = 4
-    isolated_value: float = 0.
-    init_scale_cn: float = 5.
-    init_scale_values: float = 1.
-    beta: float = 6.
+    isolated_value: float = 0.0
+    init_scale_cn: float = 5.0
+    init_scale_values: float = 1.0
+    beta: float = 6.0
     trainable: bool = True
     output_dim: int = 1
     squeeze: bool = True
@@ -133,20 +153,31 @@ class CNStore(nn.Module):
         cn = inputs[self.cn_key]
         species = inputs["species"]
 
-        cn_refs = self.param("cn_refs", nn.initializers.uniform(self.init_scale_cn), (len(D3_COV_RADII), self.store_size))[species]
-        values_refs = self.param("values_refs", nn.initializers.uniform(self.init_scale_values), (len(D3_COV_RADII), self.store_size,self.output_dim))[species]
+        cn_refs = self.param(
+            "cn_refs",
+            nn.initializers.uniform(self.init_scale_cn),
+            (len(D3_COV_RADII), self.store_size),
+        )[species]
+        values_refs = self.param(
+            "values_refs",
+            nn.initializers.uniform(self.init_scale_values),
+            (len(D3_COV_RADII), self.store_size, self.output_dim),
+        )[species]
 
         beta = self.beta
         if self.trainable:
-            beta = self.param("beta", lambda key : jnp.asarray(self.beta))
-        j=jnp.asarray(np.arange(self.n_gaussians)[None,None,:],dtype=jnp.float32)
-        delta_cns = jnp.log(jnp.sum(jnp.exp(-beta*j*((cn[:,None]-cn_refs)**2)[:,:,None]),axis=-1))
-        w = jax.nn.softmax(delta_cns,axis=-1)
+            beta = self.param("beta", lambda key: jnp.asarray(self.beta))
+        j = jnp.asarray(np.arange(self.n_gaussians)[None, None, :], dtype=jnp.float32)
+        delta_cns = jnp.log(
+            jnp.sum(
+                jnp.exp(-beta * j * ((cn[:, None] - cn_refs) ** 2)[:, :, None]), axis=-1
+            )
+        )
+        w = jax.nn.softmax(delta_cns, axis=-1)
 
-        values = jnp.sum(w[:,:,None]*values_refs,axis=1)
+        values = jnp.sum(w[:, :, None] * values_refs, axis=1)
         if self.output_dim == 1 and self.squeeze:
-            values = jnp.squeeze(values,axis=-1)
-        
+            values = jnp.squeeze(values, axis=-1)
+
         output_key = self.name if self.output_key is None else self.output_key
         return {**inputs, output_key: values}
-        
